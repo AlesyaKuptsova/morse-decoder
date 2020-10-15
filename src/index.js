ca540e694084ce8605ae95bb45939bf2a2fa333c
Alesia Kuptsova <alesya.kuptsova@tut.by>
2020-10-15 22:21:10 +0200

    feat: implement the decode function

--- src/index.js
+++ src/index.js
@@ -37,8 +37,25 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+function convert(value){
+    newArr = new Array();
+    for(var i=0; i < value.length; i++){
+        if(value[i] === '.') {
+             newArr.push('10');
+        }else if(value[i] === '-') {
+            newArr.push('11');
+        }
+    }
+    return newArr.join('').padStart(10, "0");
+}
 function decode(expr) {
-    // write your solution here
+    let MORSE_VALUE = new Object();
+
+    for (var key in MORSE_TABLE) {
+        MORSE_VALUE[convert(key)] = MORSE_TABLE[key];
+    }
+
+    return expr.match(/.{1,10}/g).map(a => a.split('**********').map(b => MORSE_VALUE[b]).join(' ')).join('');
 }
 
 module.exports = {
